d727cfe1ccaf872f91463d99b52fe3d7f18dc325
Abhishek Maurya <ab2005maurya@gmail.com>
2026-04-19 15:21:24 +0530

    Last changes

--- backend/teamfinder_config/settings.py
+++ backend/teamfinder_config/settings.py
@@ -11,8 +11,6 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '10.0.2.2',
-    '192.168.1.9',
-    '192.168.1.105',
     'teamfinder-the-mobile-app.onrender.com',
     '.onrender.com',
 ]
